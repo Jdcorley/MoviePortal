650fa1dc73199b24d25e93a95919e9372547229a
Jack <jdcorley@gmail.com>
2019-03-29 21:28:24 -0400

    added httparty to return api get request via shows_helper method

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,4 +1,7 @@
 class ShowsController < ApplicationController
+  include ShowsHelper
+
   def home
+    @popular_movies = ShowsHelper.popular_movies
   end
 end
--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -1,2 +1,9 @@
 module ShowsHelper
+  include HTTParty
+
+  def self.popular_movies
+    api_key = Rails.application.credentials.dig(:themoviedb, :api_key)
+    HTTParty.get"https://api.themoviedb.org/3/configuration?api_key=#{api_key}"
+  end
+
 end
