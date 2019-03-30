28cc52418bcba8f2933df4cafb6c3ba328d4349d
Jack <jdcorley@gmail.com>
2019-03-29 21:28:24 -0400

    added httparty to return api get request via shows_helper method

--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -1,2 +1,9 @@
 module ShowsHelper
+  include HTTParty
+
+  def self.popular_movies
+    api_key = Rails.application.credentials.dig(:themoviedb, :api_key)
+    HTTParty.get"https://api.themoviedb.org/3/discover/tv?api_key=#{api_key}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false"
+  end
+
 end
