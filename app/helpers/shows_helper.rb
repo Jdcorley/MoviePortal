3a1bb10b88bbd6e1ec24836a4934a48b472f4128
Jack <jdcorley@gmail.com>
2019-04-17 21:15:07 -0400

    added country alpha-2 to standard conversion helper method

--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -1,8 +1,9 @@
 module ShowsHelper
   include HTTParty
-
   $movie_db_api_key = Rails.application.credentials.dig(:themoviedb, :api_key)
-
+  $show_data = HTTParty.get(
+      "https://api.themoviedb.org/3/discover/tv?api_key=#{$movie_db_api_key}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false"
+  )["results"]
   $attributes = {
       1 => "name",
       2 => "vote_average",
@@ -12,21 +13,25 @@ module ShowsHelper
   }.freeze
 
   def self.popular_shows
-    show_data = HTTParty.get(
-        "https://api.themoviedb.org/3/discover/tv?api_key=#{$movie_db_api_key}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false"
-    )["results"]
-    filter_attributes(show_data)
+    fix_countries(filter_attributes($show_data))
   end
 
   def self.filter_attributes(results)
-    show_objects = []
+    show_objects = Array.new
     results.each do |show|
       show.keep_if do |k, v|
         $attributes.has_value?(k)
       end
       show_objects << show
     end
-    show_objects
+  end
+
+  def self.fix_countries(show_objects)
+    show_objects.each do |show|
+      unless show["origin_country"].nil? || show["origin_country"].first.nil?
+        show["origin_country"] = NormalizeCountry(show["origin_country"].first)
+      end
+    end
   end
 
 end
