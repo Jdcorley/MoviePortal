18e24d37fe6e630681b1c27170828300dacf0fae
Jack <jdcorley@gmail.com>
2019-04-29 13:33:46 -0400

    updated shows_controller spec

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'pry'
   gem 'rspec-rails', '~> 3.8'
+  gem 'rails-controller-testing'
 end
 
 group :development do
--- spec/controllers/shows_controller_spec.rb
+++ spec/controllers/shows_controller_spec.rb
@@ -1,18 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe ShowsController, type: :controller do
+  let(:valid_show) { Show.create!(name: "Game of Thrones")}
+  let(:invalid_show) { Show.create! }
 
   describe 'GET #index' do
+    it 'has a 200 status code' do
+      get :index
+      expect(response.status).to eq(200)
+    end
+    it 'renders shows/index view' do
+      get :index
+      expect(response).to render_template('shows/index')
+    end
   end
-
-  describe 'GET #show' do
-  end
-
 
   describe '#show_params' do
-  end
-
-  describe '#update_popular_shows' do
+    it 'requires a valid show name' do
+      expect{invalid_show}.to raise_error(ActiveRecord::RecordInvalid)
+      expect{valid_show}.not_to raise_error(ActiveRecord::RecordInvalid)
+    end
   end
 
 end
